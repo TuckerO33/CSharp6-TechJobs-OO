5d1becd99004ffec96d4bd45d09e0f3616ad20ae
Tucker Overmann <tuckerovermann@gmail.com>
2024-05-28 15:26:27 -0500

    passes Task 4 autograder tests

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -7,11 +7,38 @@ namespace TechJobs.Tests
         //Testing Objects
         //initalize your testing objects here
 
+    //Testing objects
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        
+
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            Assert.AreEqual(job1.Id, job2.Id - 1);
         }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Assert.AreEqual(job3.Name, "Product tester");
+            Assert.AreEqual(job3.EmployerName.Value, "ACME");
+            Assert.AreEqual(job3.EmployerLocation.Value, "Desert");
+            Assert.AreEqual(job3.JobType.Value, "Quality control");
+            Assert.AreEqual(job3.JobCoreCompetency.Value, "Persistence");
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse(job1.Equals(job2));
+        }
+
     }
 }
 
--- TechJobs.Test/Usings.cs
+++ TechJobs.Test/Usings.cs
@@ -1,3 +1,4 @@
 ﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using System;
 global using TechJobsOOAutoGraded6;
+global using TechJobs.Tests;
